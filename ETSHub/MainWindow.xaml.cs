4624a009cc1197f92913e5767dcd8647e3e342dc
Alex <alex.porter7@outlook.com>
2026-05-10 16:28:40 -0500

    Started working on window layout

--- ETSHub/MainWindow.xaml.cs
+++ ETSHub/MainWindow.xaml.cs
@@ -20,4 +20,9 @@ public partial class MainWindow : Window {
         InitializeComponent();
     }
 
+
+    private void EtsBuildTools_OnClick(object sender, RoutedEventArgs e) {
+        //TODO
+    }
+
 }
